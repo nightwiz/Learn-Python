ccffd776f5eb2693a4ff37187c086471643468a0
Administrator <admin@tritonsfs.com>
2017-06-07 17:54:10 +0800

    socker_client

--- Day10/gevent实现单线程下的多socket并发_client.py
+++ Day10/gevent实现单线程下的多socket并发_client.py
@@ -8,8 +8,8 @@ s.connect((HOST, PORT))
 
 while True:
     msg = bytes(input(">>:"), encoding="utf-8")     #输入数据
-    s.sendall(msg)      #send数据
-    data = s.recv(1024)     #收到数据
-    print('Recevied', repr(data))   #格式化输出
+    s.sendall(msg)                                  #send数据
+    data = s.recv(1024)                             #收到数据
+    print('Recevied', repr(data))                   #格式化输出
 
 s.close()
