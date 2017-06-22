a023ad63b0ed441c1e886f750bc91a8e2125032c
Administrator <admin@tritonsfs.com>
2017-06-22 16:17:19 +0800

    000

--- Day10/socket_client.py
+++ Day10/socket_client.py
@@ -1,14 +1,31 @@
-import socket
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-HOST = 'localhost'
-PORT = 9000
+driver = webdriver.Firefox()
+driver.get("http://baidu.com")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+element  = WebDriverWait(driver, 5, 0.5) .until(EC.presence_of_all_elements_located((By.ID, "kw")))
+#print(type(a))
+element.send_keys()
 
-while True:
-    msg = bytes(input(">>:"),encoding="utf8")
-    s.sendall(msg)
-    data = s.recv(1024)
-    print('received:', data)
-s.close()
+driver.quit()
+
+
+
+
+# import socket
+#
+# HOST = 'localhost'
+# PORT = 9000
+#
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((HOST, PORT))
+#
+# while True:
+#     msg = bytes(input(">>:"),encoding="utf8")
+#     s.sendall(msg)
+#     data = s.recv(1024)
+#     print('received:', data)
+# s.close()
