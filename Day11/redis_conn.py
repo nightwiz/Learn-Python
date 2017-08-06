2d77b9e3d08926a1a6d45db7f6c17192b85d080b
nightwiz <zhang.xu.tao@hotmail.com>
2017-08-06 22:19:07 +0800

    1

--- Day11/redis_conn.py
+++ Day11/redis_conn.py
@@ -1,19 +1,19 @@
 import redis
 
-r = redis.Redis(host='',prot=6379)
-r.set('foo','Bar')
+r = redis.Redis(host='', prot=6379)
+r.set('foo', 'Bar')
 
 print(r.get('foo'))
 
 
-###分割线###
+# ##分割线## #
 '''
 带连接池的redis conn
 '''
 
-pool = redis.ConnectionPool(host='',port=)
+pool = redis.ConnectionPool(host='', port=6379)
 
 r = redis.Redis(connection_pool=pool)
-r.set('foo','Bar')
+r.set('foo', 'Bar')
 
 print(r.get('foo'))
