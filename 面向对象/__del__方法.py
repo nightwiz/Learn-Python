353c249f592a180eaa4947300835afb015aed20b
nightwiz <zhang.xu.tao@hotmail.com>
2018-02-23 15:16:11 +0800

    __del__方法

--- 面向对象/__del__方法.py
+++ 面向对象/__del__方法.py
@@ -11,7 +11,12 @@ dog2 = dog1     # 引用Dog的值为2
 
 del dog1    # 引用Dog的值变成1
 del dog2    # 引用Dog的值变成0（引用计数为0）
+
 print("===============")
 
 # 当Dog这个类没有引用的时候，python解释器自动调用__del__方法；也就是在最后一次删除引用后__del__会被调用；
 # 在程序结束前所有的对象也会调用__del__方法释放；
+
+# 通过sys模块的getrefcount方法去测量一个对象的引用计数
+# import sys
+# sys.getrefcount(dog1)
